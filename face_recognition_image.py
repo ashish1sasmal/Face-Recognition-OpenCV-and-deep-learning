309f352956d5948ad0ab2280794c3bfcd3a931f5
ashish1sasmal <ashishsasmal1@gmail.com>
2020-07-14 04:41:40 +0530

    face recognition usinh hog classifier

--- face_recognition_image.py
+++ face_recognition_image.py
@@ -63,11 +63,12 @@ cv2.namedWindow("Image",cv2.WINDOW_NORMAL)
 
 # loop over the recognized faces
 for ((top, right, bottom, left), name) in zip(boxes, names):
-	# draw the predicted face name on the image
-	cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
-	y = top - 15 if top - 15 > 15 else top + 15
-	cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-		0.75, (0, 255, 0), 2)
+    cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
+    y = top - 15 if top - 15 > 15 else top + 15
+    cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,0.75, (0, 255, 0), 2)
+    cv2.imwrite("snape_detect.jpg",image)
+
+
 # show the output image
 cv2.imshow("Image", image)
 cv2.waitKey(0)

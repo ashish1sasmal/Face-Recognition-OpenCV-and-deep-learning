3b84c359bed7107a5771f1d14e76183c0de7125b
ashish1sasmal <ashishsasmal1@gmail.com>
2020-07-14 05:22:40 +0530

    face recognition from live video

--- face_recognition_image.py
+++ face_recognition_image.py
@@ -66,7 +66,7 @@ for ((top, right, bottom, left), name) in zip(boxes, names):
     cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
     y = top - 15 if top - 15 > 15 else top + 15
     cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,0.75, (0, 255, 0), 2)
-    cv2.imwrite("snape_detect.jpg",image)
+    # cv2.imwrite("snape_detect.jpg",image)
 
 
 # show the output image
